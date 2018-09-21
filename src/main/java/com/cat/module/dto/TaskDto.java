70f9049ff51b807e86702ecbac09495af02ff5eb
chijw <530455097@qq.com>
2018-09-21 18:06:29 +0800

    提交1

--- src/main/java/com/cat/module/dto/TaskDto.java
+++ src/main/java/com/cat/module/dto/TaskDto.java
@@ -4,36 +4,37 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 import com.cat.module.entity.BaseEntity;
+import com.cat.module.enums.ActionCode;
 import com.cat.util.NumberUtil;
 import com.cat.util.excel.annotation.ExcelField;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonInclude(Include.NON_NULL)
 @Entity
-@Table(name = "t_cat_task")
 public class TaskDto extends BaseEntity{
   
 	private String  orderId;//订单ID - 业务流水号
-	private String  realName;//用户姓名
+	private String  name;//客户姓名
 	private String  mobile;//用户手机号
 	private String  orderStatus;//订单状态
 	private BigDecimal  loanAmount;//欠款金额
 	private Integer  repayAmount;//应催金额
 	private Date repaymentTime; //到期还款日
 	private Integer overdueDays;//逾期天数
-	private String dunningTelRemark;//催收备注
-	private String dunningpeopleName;//催收人
+	private String remark;//催收备注
+	private String collectorName;//催收人
 	private Date payoffTime;//还清日期
 	private Long organizationId;//机构id
 	private Long userId;//催收员id
-	
+	private ActionCode actionFeedbackType;//行动码
 	
 	//------------请求参数----------
 	private Integer overdueDaysStart;//逾期天数第1个值
 	private Integer overdueDaysEnd;//逾期天数第2个值
+	private Date payoffTimeStart;//还清日期第一个值
+	private Date payoffTimeEnd;//还清日期第二个值
 	
 	@ExcelField(title = "订单编号", type = 1, align = 2, sort = 110)
 	public String getOrderId() {
@@ -43,11 +44,11 @@ public class TaskDto extends BaseEntity{
 		this.orderId = orderId;
 	}
 	@ExcelField(title = "姓名", type = 1, align = 2, sort = 10)
-	public String getRealName() {
-		return realName;
+	public String getName() {
+		return name;
 	}
-	public void setRealName(String realName) {
-		this.realName = realName;
+	public void setName(String name) {
+		this.name = name;
 	}
 	@ExcelField(title = "手机号", type = 1, align = 2, sort = 20)
 	public String getMobile() {
@@ -66,7 +67,7 @@ public class TaskDto extends BaseEntity{
 	public void setOrderStatus(String orderStatus) {
 		this.orderStatus = orderStatus;
 	}
-	@ExcelField(title = "欠款金额", type = 1, align = 2, sort = 30)
+	@ExcelField(title = "欠款金额", type = 1, align = 2, sort = 40)
 	public String getLoanAmountText() {
 		return null != this.loanAmount ? NumberUtil.formatTosepara(this.loanAmount) : "";
 	}
@@ -76,7 +77,7 @@ public class TaskDto extends BaseEntity{
 	public void setLoanAmount(BigDecimal loanAmount) {
 		this.loanAmount = loanAmount;
 	}
-	@ExcelField(title = "应催金额", type = 1, align = 2, sort = 40)
+	@ExcelField(title = "应催金额", type = 1, align = 2, sort = 30)
 	public String getRepayAmountText() {
 		return null != this.repayAmount ? NumberUtil.formatTosepara(this.repayAmount) : "";
 	}
@@ -101,18 +102,18 @@ public class TaskDto extends BaseEntity{
 		this.overdueDays = overdueDays;
 	}
 	@ExcelField(title = "催收备注", type = 1, align = 2, sort = 80)
-	public String getDunningTelRemark() {
-		return dunningTelRemark;
+	public String getRemark() {
+		return remark;
 	}
-	public void setDunningTelRemark(String dunningTelRemark) {
-		this.dunningTelRemark = dunningTelRemark;
+	public void setRemark(String remark) {
+		this.remark = remark;
 	}
 	@ExcelField(title = "催收人", type = 1, align = 2, sort = 90)
-	public String getDunningpeopleName() {
-		return dunningpeopleName;
+	public String getCollectorName() {
+		return collectorName;
 	}
-	public void setDunningpeopleName(String dunningpeopleName) {
-		this.dunningpeopleName = dunningpeopleName;
+	public void setDunningpeopleName(String collectorName) {
+		this.collectorName = collectorName;
 	}
 	@ExcelField(title = "还清日期", type = 1, align = 2, sort = 100)
 	public Date getPayoffTime() {
