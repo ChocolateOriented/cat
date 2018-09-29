79891a104771188e80ef1933b57b62374d4cd4dd
chijw <530455097@qq.com>
2018-09-29 17:49:38 +0800

    减免ing

--- src/main/java/com/cat/manager/ReliefAmountManager.java
+++ src/main/java/com/cat/manager/ReliefAmountManager.java
@@ -3,11 +3,11 @@ package com.cat.manager;
 import com.cat.module.dto.SuonaMessageDto;
 import com.cat.module.dto.result.Results;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-//@FeignClient(url = "${feignClient.raptor.url}", name = "reliefAmount")
+@FeignClient(url = "${feignClient.raptor.url}", name = "reliefAmount")
 public interface ReliefAmountManager {
 
-//  @GetMapping("/ss/")
+  @PostMapping("/coupon/create ")
   Results send(SuonaMessageDto message);
 }
