f27475c1c07d291afd1a28a9002e609031a12a3a
chijw <530455097@qq.com>
2018-09-30 15:12:07 +0800

    减免金额添加盐

--- src/main/java/com/cat/manager/RaptorManager.java
+++ src/main/java/com/cat/manager/RaptorManager.java
@@ -1,8 +1,10 @@
 package com.cat.manager;
 
 import com.cat.module.dto.BaseResponse;
-import com.cat.module.dto.ReliefAmountDto;
 import com.cat.module.dto.result.Results;
+
+import java.util.Map;
+
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface RaptorManager {
 
   @PostMapping("/coupon/create ")
-  BaseResponse send(ReliefAmountDto message);
+  BaseResponse send(Map<String, String> params);
 
   @GetMapping("/outside/to_black_user ")
   Results blacklistCustomer(@RequestParam("userCode") String userCode
