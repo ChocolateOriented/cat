9147ff8033ea367e2f169912db3d68c7170919f6
chijw <530455097@qq.com>
2018-10-12 13:27:38 +0800

    减免和拉黑添加权限

--- src/main/java/com/cat/web/CustomerController.java
+++ src/main/java/com/cat/web/CustomerController.java
@@ -2,10 +2,16 @@ package com.cat.web;
 
 import com.cat.module.dto.result.ResultConstant;
 import com.cat.module.dto.result.Results;
+import com.cat.module.entity.User;
+import com.cat.module.enums.Role;
 import com.cat.module.dto.BlackListDto;
 import com.cat.module.vo.OrderInfo;
+import com.cat.repository.UserRepository;
 import com.cat.service.CustomerService;
 import com.cat.service.DisposeOrderAndCustomerInfoService;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -23,7 +29,8 @@ public class CustomerController extends BaseController{
     private DisposeOrderAndCustomerInfoService disposeCustomerInfoService;
     @Autowired
     private CustomerService customerService;
-
+	@Autowired 
+	private	UserRepository userRepository;
     @GetMapping("get_customer_and_order_info")
     public Results getCustomerInfo(@RequestParam("orderId") String orderId) {
         try {
@@ -40,10 +47,21 @@ public class CustomerController extends BaseController{
     }
 
     @PostMapping("blacklist_customer")
-    public Results blackList(@Validated @RequestBody BlackListDto blackListDto,BindingResult bindingResult){
+    public Results blackList(@Validated @RequestBody BlackListDto blackListDto,BindingResult bindingResult, HttpServletRequest request){
       if (bindingResult.hasErrors()) {
         return new Results(ResultConstant.EMPTY_PARAM, getFieldErrorsMessages(bindingResult));
       }
+        String userId = request.getHeader("User-Id");
+		User user = userRepository.findOne(userId);
+		if(user == null){
+			logger.warn("该用户不存在,userID={}",userId);
+			return new Results(-1L,"您没有权限");
+		}
+		Role role = user.getRole() ;
+		if(role == null || role == Role.COLLECTOR ){
+			logger.warn("该用户没分配角色,userID={}",userId);
+			return new Results(-1L,"您没有权限");
+		}
       try {
         customerService.blackList(blackListDto);
       }catch (Exception e){
