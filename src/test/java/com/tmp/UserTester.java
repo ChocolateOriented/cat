31ffea4f5fd100aa29c78b9235de2909fb9ee760
lijx <lijx@mo9.com>
2018-10-16 10:39:36 +0800

    重置密码暂停

--- src/test/java/com/tmp/UserTester.java
+++ src/test/java/com/tmp/UserTester.java
@@ -1,11 +1,14 @@
 package com.tmp;
 
 import com.cat.CatApplication;
+import com.cat.module.dto.BlackListDto;
 import com.cat.module.dto.DivisionUserDto;
 import com.cat.module.entity.User;
 import com.cat.module.enums.UserStatus;
 import com.cat.repository.TaskRepository;
 import com.cat.repository.UserRepository;
+import com.cat.service.AccountService;
+import com.cat.service.CustomerService;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,7 +27,17 @@ public class UserTester {
 	private UserRepository userRepository;
 	@Autowired
 	private TaskRepository taskRepository;
-	
+	@Autowired
+	private CustomerService customerService;
+	@Autowired
+	private AccountService accountService;
+
+	@Test
+	public void sendResetPasswordToken() throws Exception {
+		accountService.sendResetPasswordToken("jxli@mo9.com");
+	}
+
+
 	@Test
 	public void testSearch() {
 		List<DivisionUserDto> users = taskRepository.findPeopleSumcorpusamountByDunningcycle("Q1");
@@ -51,6 +64,26 @@ public class UserTester {
 			}
 			user.setCollectCycle(collectCycle);
 			userRepository.save(user);
+		}
+	}
+	@Test
+	public void blacklist(){
+		BlackListDto blackDto = new BlackListDto();
+		blackDto.setCustomerId("AA20A480E526D644D13D9AC5593D2688");
+		blackDto.setReason("测试");
+		customerService.blackList(blackDto);
+	}
+
+	public static void main(String[] args) {
+		for (int i=0;i<40;i++){
+			String collectCycle;
+			String sql = "INSERT INTO `cat`.`t_user` (`id`, `create_by`, `create_time`, `update_by`, "
+				+ "`update_time`, `collect_cycle`, `name`, `organization_id`, `status`, `email`, `role`,"
+				+ " `auto_division`, `password`) VALUES ("
+				+ "'49525123342716108"+i+"', 'sys', '2018-09-28 15:11:35', 'sys', '2018-09-28 15:11:35',"
+				+ " 'Q0,Q1', 'caozihao', '1', 'NORMAL', '154791107@qq.com', 'COLLECTOR', '1',"
+				+ " '2e36656bbee6a943bfa0ee0c64e88bed4831d2a8');";
+			System.out.println(sql);
 		}
 	}
 }
